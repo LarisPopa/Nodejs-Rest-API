0a41a5517c386753372eae2fee734193289e4bec
LarisPopa <popa.laris@yahoo.ro>
2019-10-06 22:16:14 +0300

    Add new tests

--- test/addUser.js
+++ test/addUser.js
@@ -31,4 +31,79 @@ describe('POST /addUser/id', () => {
         })
         .catch(err=>done(err))    
     });
+    it('Test if email is invalid', (done) => {
+        //count users before added new user
+        let numberOfUsersBeforeAdded=0
+        User.countDocuments({}).exec((err, count) => {
+            if (err) {
+                res.send(err);
+                return;
+            }
+           numberOfUsersBeforeAdded=count
+        });
+        request(app).post('/addUser')
+        .send({name: "NewUser", age:23, email:"", friends:["lrs", "ion", "ana"]})
+        .then(()=>{
+            //compare number of users before added with new number of users (which will be the same, because user will not added)
+            User.countDocuments({}).exec((err, count) => {
+                if (err) {
+                    res.send(err);
+                    return;
+                }
+                expect(numberOfUsersBeforeAdded).to.equal(count);
+                done();
+            });
+        })
+        .catch(err=>done(err))    
+    });
+    it('Test if name is invalid', (done) => {
+        //count users before added new user
+        let numberOfUsersBeforeAdded=0
+        User.countDocuments({}).exec((err, count) => {
+            if (err) {
+                res.send(err);
+                return;
+            }
+           numberOfUsersBeforeAdded=count
+        });
+        request(app).post('/addUser')
+        .send({name: "", age:23, email:"ion@ion.com", friends:["lrs", "ion", "ana"]})
+        .then(()=>{
+            //compare number of users before added with new number of users (which will be the same, because user will not added)
+            User.countDocuments({}).exec((err, count) => {
+                if (err) {
+                    res.send(err);
+                    return;
+                }
+                expect(numberOfUsersBeforeAdded).to.equal(count);
+                done();
+            });
+        })
+        .catch(err=>done(err))    
+    });
+    it('Test if age is not a number', (done) => {
+        //count users before added new user
+        let numberOfUsersBeforeAdded=0
+        User.countDocuments({}).exec((err, count) => {
+            if (err) {
+                res.send(err);
+                return;
+            }
+           numberOfUsersBeforeAdded=count
+        });
+        request(app).post('/addUser')
+        .send({name: "ion", age:"qwd", email:"ion@ion.com", friends:["lrs", "ion", "ana"]})
+        .then(()=>{
+            //compare number of users before added with new number of users (which will be the same, because user will not added)
+            User.countDocuments({}).exec((err, count) => {
+                if (err) {
+                    res.send(err);
+                    return;
+                }
+                expect(numberOfUsersBeforeAdded).to.equal(count);
+                done();
+            });
+        })
+        .catch(err=>done(err))    
+    });
 })
